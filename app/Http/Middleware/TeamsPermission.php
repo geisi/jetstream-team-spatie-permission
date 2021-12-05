6eec3eeb3fe2b55ef71d45132a9317152eb623e4
geisi <tim@partysturmevents.de>
2021-12-05 14:08:51 +0100

    add TeamsPermission middleware

---
+++ app/Http/Middleware/TeamsPermission.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Spatie\Permission\PermissionRegistrar;
+
+class TeamsPermission
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!empty($user = auth()->user()) && !empty($user->current_team_id)) {
+            app(PermissionRegistrar::class)->setPermissionsTeamId($user->current_team_id);
+        }
+
+        return $next($request);
+    }
+}
